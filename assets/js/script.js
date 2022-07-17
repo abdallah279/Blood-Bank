42a34d646eaf68fe2d348259d1bf5698ec092fbb
abdallah zeko <abdallahzeko8@gmail.com>
2022-07-17 20:33:48 +0200

    Push Project

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".articles-carousel").owlCarousel({
+        loop:true,
+        margin:50,
+        rtl: true,
+        nav:true,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
